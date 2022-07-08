e6eff0508467da087d71fcd1842ff61b95fc49d7
Pablo Camino <pablo@cloudflare.com>
2022-07-08 16:21:47 -0500

    Add privacy policy

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,7 +1,11 @@
 <div
-	class="grid grid-cols-1 font-mon font-bold text-2xl border-t-2 w-full sticky overflow-hidden self-end"
+	class="grid grid-cols-3 font-mon font-bold text-2xl border-t-2 w-full sticky overflow-hidden self-end"
 >
+	<div />
 	<p class=" justify-self-center m-2">
 		Built by Th3st1gh powered by <a href="https://kit.svelte.dev/"> Svelte</a>
+	</p>
+  <p class=" justify-self-center m-2 text-xs">
+	 <a href="/privatepolicy"> Privacy policy </a>
 	</p>
 </div>
